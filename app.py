c53b4977d467ae6a8c171f0ef493e277942a9300
cyferdev <mrnzero321@gmail.com>
2023-10-13 01:10:04 +0530

    f

--- app.py
+++ app.py
@@ -2,26 +2,43 @@ import os
 import openai
 from dotenv import load_dotenv
 from persona import persona
+
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-prompt = "what do you think about griffith"
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {
-      "role": "system",
-      "content": f"{persona}"
-    },
-    {
-        "role":"assistant",
-        "content": f"{prompt}"
-    }
-  ],
-  temperature=1,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
-print(response.choices[0])
+
+def main():
+  def get_response(prompt):
+      response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {
+          "role": "system",
+          "content": f"{persona}"
+        },
+        {
+            "role":"assistant",
+            "content": f"{prompt}"
+        }
+      ],
+      temperature=1,
+      max_tokens=256,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+      
+      print(response.choices)
+  
+  reply = ""
+  while(reply == ""):
+      
+      prompt = input("Enter : (Type /quit to quit)")
+      reply = prompt
+      if prompt =="/quit":
+         break
+      get_response(prompt)
+      reply = ""
+      
+if __name__ == "__main__":
+   main()
