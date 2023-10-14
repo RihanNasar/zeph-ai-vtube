9ebf92ea1dd66931ab204adf280b87c6953c3d57
cyferdev <mrnzero321@gmail.com>
2023-10-14 14:24:07 +0530

    conversation history

--- app.py
+++ app.py
@@ -6,21 +6,15 @@ from persona import persona
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
+messages= [{
+          "role": "system",
+          "content": f"{persona}"
+        },]
 def main():
   def get_response(prompt):
       response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
-      messages=[
-        {
-          "role": "system",
-          "content": f"{persona}"
-        },
-        {
-            "role":"assistant",
-            "content": f"{prompt}"
-        }
-      ],
+      messages=messages,
       temperature=1,
       max_tokens=256,
       top_p=1,
@@ -28,17 +22,27 @@ def main():
       presence_penalty=0
     )
       
-      print(response.choices)
+      response = response.choices[0].message.content
+      return response
   
   reply = ""
   while(reply == ""):
-      
-      prompt = input("Enter : (Type /quit to quit)")
+      prompt = input("(Type /quit to quit) \n Enter: ")
       reply = prompt
       if prompt =="/quit":
          break
-      get_response(prompt)
+      user_content = {"role": "user","content":prompt}
+      messages.append(user_content)
+      response = get_response(prompt)
+      
+      ai_content = {"role": "assistant", "content": response}
+      print(response)
+      
+      messages.append(ai_content)
+      
       reply = ""
+      
+     
       
 if __name__ == "__main__":
    main()
