81a3018ae9b65f98a6ad0d8c65c660acd68a8f0a
cyferdev <mrnzero321@gmail.com>
2023-10-21 00:46:08 +0530

    added tts using elevenlabs

--- app.py
+++ app.py
@@ -2,14 +2,41 @@ import os
 import openai
 from dotenv import load_dotenv
 from persona import persona
+from elevenlabs import voices,set_api_key,Voice,VoiceSettings,generate,save,VoiceDesign,Gender,Age,Accent
+
+
 
 load_dotenv()
-
+os.environ['PATH'] += os.pathsep + "C:/ffmpeg/bin"
+set_api_key(os.getenv("ELEVENLABS_API_KEY"))
+voice = voices()
+print(voice[-1])
 openai.api_key = os.getenv("OPENAI_API_KEY")
 messages= [{
           "role": "system",
           "content": f"{persona}"
         },]
+def generate_audio(response):
+    design = VoiceDesign(
+    name='Lexa',
+    text=response,
+    voice_description="A voice resonating from the depths, textured with a rough, gravelly quality, and charged with an intense determination that effortlessly conveys the intricate tapestry of emotions , he talks slowly.",
+    gender=Gender.male,
+    age=Age.middle_aged,
+    accent=Accent.american,
+    accent_strength=1.4,
+    )
+    audio = design.generate()
+    
+    # audio = generate(
+    # text=response,
+    # voice=Voice(
+    #     voice_id='MYwiFnEo7h6BOxShlxng',
+    #     settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
+    # )
+    # )
+    save(audio,filename="audio.wav")
+
 def main():
   def get_response(prompt):
       response = openai.ChatCompletion.create(
@@ -37,7 +64,7 @@ def main():
       
       ai_content = {"role": "assistant", "content": response}
       print(response)
-      
+      generate_audio(response)
       messages.append(ai_content)
       
       reply = ""
